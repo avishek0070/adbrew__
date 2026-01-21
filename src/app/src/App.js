9f58c4bf197efed6507f0cfa795379400bb2afed
avishek0007 <pes1202202287@pesu.pes.edu>
2026-01-21 10:40:07 +0530

    Complete Adbrew assignment: React hooks + Django API + Mongo via Docker

--- src/app/src/App.js
+++ src/app/src/App.js
@@ -1,27 +1,54 @@
-import './App.css';
-import logo from './logo.svg';
+import { useEffect, useState } from "react";
+import "./App.css";
 
+function App() {
+  const [todos, setTodos] = useState([]);
+  const [text, setText] = useState("");
 
-export function App() {
+  const fetchTodos = async () => {
+    const res = await fetch("http://localhost:8000/todos/");
+    const data = await res.json();
+    setTodos(Array.isArray(data) ? data : []);
+  };
+
+  useEffect(() => {
+    fetchTodos();
+  }, []);
+
+  const submitTodo = async (e) => {
+    e.preventDefault();
+    if (!text.trim()) return;
+
+    await fetch("http://localhost:8000/todos/", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ text }),
+    });
+
+    setText("");
+    fetchTodos();
+  };
+
   return (
     <div className="App">
-      <div>
-        <h1>List of TODOs</h1>
-        <li>Learn Docker</li>
-        <li>Learn React</li>
-      </div>
-      <div>
-        <h1>Create a ToDo</h1>
-        <form>
-          <div>
-            <label for="todo">ToDo: </label>
-            <input type="text" />
-          </div>
-          <div style={{"marginTop": "5px"}}>
-            <button>Add ToDo!</button>
-          </div>
-        </form>
-      </div>
+      <h1>List of TODOs</h1>
+
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo.id}>{todo.text}</li>
+        ))}
+      </ul>
+
+      <h1>Create a TODO</h1>
+
+      <form onSubmit={submitTodo}>
+        <input
+          type="text"
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+        />
+        <button>Add TODO</button>
+      </form>
     </div>
   );
 }
--- src/rest/rest/views.py
+++ src/rest/rest/views.py
@@ -1,20 +1,47 @@
-from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-import json, logging, os
+import os
 from pymongo import MongoClient
+from bson import ObjectId
 
-mongo_uri = 'mongodb://' + os.environ["MONGO_HOST"] + ':' + os.environ["MONGO_PORT"]
-db = MongoClient(mongo_uri)['test_db']
+mongo_uri = f'mongodb://{os.environ["MONGO_HOST"]}:{os.environ["MONGO_PORT"]}'
+client = MongoClient(mongo_uri)
+db = client["test_db"]
+todos_collection = db["todos"]
+
 
 class TodoListView(APIView):
 
     def get(self, request):
-        # Implement this method - return all todo items from db instance above.
-        return Response({}, status=status.HTTP_200_OK)
-        
-    def post(self, request):
-        # Implement this method - accept a todo item in a mongo collection, persist it using db instance above.
-        return Response({}, status=status.HTTP_200_OK)
+        todos = []
+        for todo in todos_collection.find():
+            todos.append({
+                "id": str(todo["_id"]),   # IMPORTANT
+                "text": todo.get("text", "")
+            })
 
+        return Response(todos, status=status.HTTP_200_OK)
+
+    def post(self, request):
+        data = request.data
+
+        if "text" not in data or not data["text"].strip():
+            return Response(
+                {"error": "Todo text is required"},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+        todo = {
+            "text": data["text"]
+        }
+
+        result = todos_collection.insert_one(todo)
+
+        return Response(
+            {
+                "id": str(result.inserted_id),
+                "text": data["text"]
+            },
+            status=status.HTTP_201_CREATED
+        )
